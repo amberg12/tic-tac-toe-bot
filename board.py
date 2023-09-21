45dfc1c59fa1360763c08b716b99fb9d5bff0e9e
amberg12 <a9870185@gmail.com>
2023-09-21 19:54:17 +0100

    Create board.py
    Create board.py
    
    Contains basic object for a board with the ability to process moves and generate moves

---
+++ board.py
@@ -0,0 +1,30 @@
+class Board:
+    def __init__(self, board=False, move=None):
+        if board == False:
+            self.x_board = 0b000000000
+            self.o_board = 0b000000000
+            self.x_plays = True
+        else:
+            self.x_board = board.x_board
+            self.o_board = board.o_board
+            self.x_plays = board.x_plays
+            self.process_move(move)
+
+    def process_move(self, move):
+        if self.x_plays:
+            self.x_board = self.x_board | move
+        else:
+            self.o_board = self.o_board | move
+
+        self.x_plays = not self.x_plays
+
+    def generate_moves(self):
+        combined_board = self.x_board | self.y_board
+        move_pointer = 0b100000000
+        moves = []
+
+        for i in range(9):
+            if (move_pointer >> i) & combined_board == (move_pointer >> i):
+                moves.append(move_pointer)
+
+        return moves
