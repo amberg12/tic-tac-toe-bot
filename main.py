9ff0ee6e23b70425ca4e48873e12ae828cf245e5
amberg12 <145551321+amberg12@users.noreply.github.com>
2023-09-23 19:46:52 +0100

    added simple player interaction
    players can now play against themselves

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import board
 import minimax
+import player
 
 if __name__ == "__main__":
     current_board = board.Board()
+    player_interaface = player.Player()
+    while current_board.generate_evaluation() == None:
+        current_board.display_board()
+        current_board = board.Board(board=current_board, move=player_interaface.get_player_response(current_board.generate_moves()))
+    
+    current_board.display_board()
---
+++ player.py
@@ -0,0 +1,18 @@
+import math
+
+class Player:
+    def __init__(self):
+        pass
+
+    def generate_player_readable_moves(self, computer_readable_moves):
+        return [str(int(9-math.log2(move))) for move in computer_readable_moves]
+    
+    def generate_computer_readable_moves(self,  player_readable_moves):
+        return [0b1 << int(player_readable_move) - 1 for player_readable_move in player_readable_moves]
+    
+    def get_player_response(self, computer_readable_moves):
+        player_readable_moves = self.generate_player_readable_moves(computer_readable_moves)
+        player_input = ""
+        while player_input not in player_readable_moves:
+            player_input = input(f"Please enter the move you would like to play in {player_readable_moves}: ")
+        return self.generate_computer_readable_moves(player_input)[0]
