94e830a3b979480c3e99f58652b48ea71fbd0266
amberg12 <145551321+amberg12@users.noreply.github.com>
2023-09-23 22:08:11 +0100

    added caching of values
    added caching of values. reduced total time by 3x

--- README.md
+++ README.md
@@ -3,11 +3,25 @@
 
  effects of algorithm optimisation:
  simple minimax:
+
  initial move "1": 1188 ms
+
  initial move "2": 1281 ms
+
  initial move "5": 1563 ms
 
  alpha/beta pruning:
+ 
  initial move "1":  141 ms
+
  initial move "2":  266 ms
+
  initial move "5":  156 ms
+
+ cache:
+
+ initial move "1":   47 ms
+
+ initial move "2":   47 ms
+
+ initial move "3":   31 ms
--- minimax.py
+++ minimax.py
@@ -40,9 +40,13 @@ class MiniMax_Search:
         print(f"{1000*(time.process_time() - initial_time)}ms to calculate.")
         return best_move
 
-    def minimax(self, node, is_maxxing, depth=10, alpha=-9999, beta=9999):
+    def minimax(self, node, is_maxxing, depth=10, alpha=-9999, beta=9999, cache={}):
+
+        if cache.get(node.x_board << 9 + node.o_board, None) != None:
+            return cache.get(node.x_board << 9 + node.o_board, None)
 
         if node.generate_evaluation() != None:
+            cache[node.x_board << 9 + node.o_board] = node.generate_evaluation()
             return node.generate_evaluation()
         
         if depth == 0:
@@ -53,7 +57,9 @@ class MiniMax_Search:
         if is_maxxing == True:
             value = -9999
             for child in self.children:
-                value = max(value, self.minimax(child, False, depth=depth-1, alpha=alpha, beta=beta))
+                child_value = self.minimax(child, False, depth=depth-1, alpha=alpha, beta=beta)
+                cache[child.x_board << 9 + child.o_board] = child_value
+                value = max(value, child_value)
                 alpha = max(alpha, value)
                 if beta <= alpha:
                     break
@@ -61,7 +67,9 @@ class MiniMax_Search:
         else:
             value = 9999
             for child in self.children:
-                value = min(value, self.minimax(child, True, depth=depth-1, alpha=alpha, beta=beta))
+                child_value = self.minimax(child, True, depth=depth-1, alpha=alpha, beta=beta)
+                cache[child.x_board << 9 + child.o_board] = child_value
+                value = min(value, child_value)
                 beta = min(beta, value)
                 if beta <= alpha:
                     break
